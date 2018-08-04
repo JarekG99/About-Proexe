117e228433bbee08cfdc23cf2e64c7c539eb7a7b
Jarek Gajdulewicz <j.gajdulewicz@icloud.com>
2018-08-04 17:27:41 +0200

    Media & some jQuery

--- scripts.js
+++ scripts.js
@@ -3,6 +3,11 @@
 $(function() {
     $("#datepicker").datepicker({
         minDate: new Date(),
+
+        onSelect: function() {
+            dateObject= $(this).datepicker('getDate', 'minDate');
+        },
+
         nextText: "&gt",
         prevText: "&lt"
     });
@@ -24,5 +29,19 @@ $city.on('change', function() {
         $hotel.append($("<option></option>").text(hotel));
     })
 }).trigger('change');
-
 });
+
+let email = $("#email").val();
+
+function isEmail(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+};
+
+
+function printValues() {
+  let values = JSON.stringify(($(":input").serializeArray()).push(dateObject));
+  console.log(values);
+};
+
+$(":button").click( printValues );
