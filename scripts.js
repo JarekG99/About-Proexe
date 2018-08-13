65934bb357df4095b1f30318097020d6ac440854
Jarek Gajdulewicz <j.gajdulewicz@icloud.com>
2018-08-13 18:51:07 +0200

    Changes in Js and CSS

--- scripts.js
+++ scripts.js
@@ -1,15 +1,26 @@
 // scripts.js
 
+$().ready(function() {
+var $notes = $("#notes");
+$(".note").each(function(ind, note) {
+var $note = $(note);
+var text = $note.text();
+$note.text("(" + (ind + 1) + ")");
+$notes.append($("<li></li>").text(text));
+});
+})
+
+
 $(function() {
     $("#datepicker").datepicker({
         minDate: new Date(),
-
+        
         onSelect: function() {
             dateObject= $(this).datepicker('getDate', 'minDate');
+            date = dateObject.getFullYear() + "/" + (dateObject.getMonth() + 1) + "/" + dateObject.getDate()
+            let day = dateObject.getDate();
         },
 
-        nextText: "&gt",
-        prevText: "&lt"
     });
 
 
@@ -31,17 +42,54 @@ $city.on('change', function() {
 }).trigger('change');
 });
 
-let email = $("#email").val();
+var blockedDays = [12, 15, 20, 26, 27];
 
-function isEmail(email) {
-    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    return regex.test(email);
-};
+// $().ready(function() {
+//   $.each(blockedDays, function() {
+//     $day = $().datepicker('getDate');
+//     console.log($day)
+//       if($(this) === $day) {
+//         $day.addClass("blocked ui-state-disabled")
+//     };
+//   });
+// })
+
+  let email = $("#email").val();
+
+  function validateEmail(email) {
+    // var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    let filter = /[^@]+@[^@]+/
+    if (filter.test(email)) {
+      return true;
+    }
+    else {
+        return alert("Pls enter the valid e-mail");
+    }
+  }
+
+  $('#email').blur(function() {
+       if ($.trim(email).length == 0) {
+           alert('Please enter valid email address');
+           e.preventDefault();
+       }
+       if (validateEmail(email)) {
+           alert('Email is valid');
+           e.preventDefault();
+       }
+   });
+
 
 
 function printValues() {
-  let values = JSON.stringify(($(":input").serializeArray()).push(dateObject));
+  let values = {date};
+  $(":input").each(function() {
+    values[$(this).attr("name")] = $(this).val();
+  });
   console.log(values);
+  console.log(JSON.stringify(values));
 };
-
-$(":button").click( printValues );
+$("#email").on()
+$(document).on("click", "#button", function() {
+    validateEmail();
+    printValues();
+});
